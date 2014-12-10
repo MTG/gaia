61ffd17c521db27c03b20c883f08a1ad7d0c488d
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2014-12-10 20:58:22 +0100

    Generate classification project files from groundtruth and filelist

--- src/bindings/pygaia/scripts/classification/generate_classification_project.py
+++ src/bindings/pygaia/scripts/classification/generate_classification_project.py
@@ -21,84 +21,66 @@
 
 
 from __future__ import with_statement
-import os, sys
-from os.path import join, abspath, splitext, exists
+import sys
+from os.path import join, abspath
 from optparse import OptionParser
-import gaia2.fastyaml as yaml
-import gaia2.mtgdb
-from gaia2.classification import GroundTruth
 from gaia2 import filedir
-
+import gaia2.fastyaml as yaml
 
 PROJECT_TEMPLATE = open(join(filedir(), 'classification_project_template.yaml')).read()
 
-def validEqloud(f):
-    return f.endswith('.sig') and not f.endswith('.neq.sig')
-
-def validNeqloud(f):
-    return f.endswith('.neq.sig')
 
 
-def sigfileListFromCollection(collection, sigfiles_dir):
-    # generate the filelist for the .sig files, as what we have in the metadata/ folder is
-    # the filelist for the audio files
-    # NB: we only take the first audio format available here (still true?)
-    audioFiles = collection.relativePathFiles()
-
-    # NB: we only take the files which are present in the groundtruth, as the others are
-    #     useless to us (and probably amount to noise, even if we were able to deal with them)
-
-    sigfileList = dict((pid, abspath(join(sigfiles_dir, f) + '.sig'))
-                       for pid, f in audioFiles.items()
-                       if pid in collection.groundTruth)
-
-    return sigfileList
-
-
-
-
-def generateProjectFromCollection():
-    parser = OptionParser(usage = '%prog [options] collection_name sigfiles_dir project_file\n\n' +
-                          'this will also generate a groundtruth and a filelist file to be used by the project file.')
-
-    parser.add_option('-g', '--groundtruth', dest = 'desiredGroundTruth',
-                      help = 'Which type of ground truth to use, in case the collection has more than one')
+def generateProject():
+    parser = OptionParser(usage = '%prog [options] groundtruth_file filelist_file project_file')
 
     options, args = parser.parse_args()
 
     try:
-        collection_name = args[0]
-        sigfiles_dir = args[1]
+        groundtruth_file = args[0]
+        filelist_file = args[1]
         project_file = args[2]
     except:
         parser.print_help()
         sys.exit(1)
 
-    # create collection from a directory collection_name if it exists, use an MTG-DB collection otherwise
-    if os.path.isdir(collection_name):
-        collec = gaia2.mtgdb.Collection(collection_name, groundTruth = options.desiredGroundTruth)
-    else:
-        collec = gaia2.mtgdb.MtgdbCollection(collection_name, groundTruth = options.desiredGroundTruth)
+    gt = yaml.load(open(groundtruth_file, 'r'))
+    try:
+        className = gt['className']
+        groundTruth = gt['groundTruth']   
+    except:
+        print groundtruth_file, "groundtruth file has incorrect format"
+        sys.exit(2)
 
-    # write yaml file of sigfiles to merge for this project
-    filelistFilename = abspath(splitext(project_file)[0] + '.filelist.yaml')
-    sigfileList = sigfileListFromCollection(collec, sigfiles_dir)
+    fl = yaml.load(open(filelist_file, 'r'))
 
-    with open(filelistFilename, 'w') as filelist:
-        yaml.dump(sigfileList, filelist)
+    gt_trackids = groundTruth.keys()
+    fl_trackids = fl.keys()
+
+    # check that there are no dublicate ids 
+    if len(gt_trackids) != len(set(gt_trackids)):
+        print groundtruth_file, "contains dublicate track ids"
+        sys.exit(3)
+    
+    if len(fl_trackids) != len(set(fl_trackids)):
+        print filelist_file, "contains dublicate track ids"
+        sys.exit(3)  
+
+    # check if filelist is consistent with groundtruth (no files missing)
+    if set(gt_trackids) != set(fl_trackids):
+        print "track ids found in", groundtruth_file, "are inconsistent with", filelist_file
+        sys.exit(4)
 
     # write the project file
     with open(project_file, 'w') as pfile:
-        pfile.write(PROJECT_TEMPLATE % { 'className': collec.groundTruth.className,
-                                         'filelist': filelistFilename,
-                                         'groundtruth': abspath(collec._groundTruthFile) })
+        pfile.write(PROJECT_TEMPLATE % { 'className': className,
+                                         'filelist': abspath(filelist_file),
+                                         'groundtruth': abspath(groundtruth_file) })
 
     print 'Successfully written', project_file
 
 
 
-
-
 if __name__ == '__main__':
-    generateProjectFromCollection()
+    generateProject()
 
