ba668acfc38848f936fadba48a007202e3f05e1c
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2015-04-24 17:40:33 +0200

    Add scripts for balancing classification datasets

---
+++ src/bindings/pygaia/scripts/classification/balanced_sampling.py
@@ -0,0 +1,35 @@
+# This script creates a balanced ground truth given an unbalanced on by applying 
+# random sampling. The size of the resulting classes equals to the minimum size 
+# among original classes.
+
+import sys
+import yaml
+from random import shuffle
+
+try:
+    input_gt = sys.argv[1]
+    balanced_gt = sys.argv[2]
+except:
+    print 'usage:', sys.argv[0], '<input-grounttruth> <output-balanced-groundtruth>'
+    sys.exit()
+
+input_gt = yaml.load(open(input_gt, 'r'))
+
+gt = {}
+for t, l in input_gt['groundTruth'].items():
+    gt.setdefault(l, [])
+    gt[l] += [t]
+
+for label in gt:
+    print label, len(gt[label])
+
+min_class_len = min(len(gt[label]) for label in gt)
+print 'Minimum class length:', min_class_len
+
+for label in gt:
+    shuffle(gt[label])
+    for track in gt[label][:min_class_len]:
+        input_gt['groundTruth'][track] = label
+
+with open(balanced_gt, 'w') as f:
+    yaml.dump(input_gt, f)
---
+++ src/bindings/pygaia/scripts/classification/filter_filelist_by_groundtruth.py
@@ -0,0 +1,24 @@
+# This script filters descriptors filelist removing mapping for 
+# all tracks that are not present in the ground truth file
+
+import sys, yaml
+
+try:
+    gt_file = sys.argv[1]
+    filelist_file = sys.argv[2]
+    results_filelist_file = sys.argv[3]
+except:
+    print 'usage:', sys.argv[0], '<ground-truth-file> <filelist-file> <result-filelist-file>'
+    sys.exit()
+
+gt = yaml.load(open(gt_file, 'r'))
+filelist = yaml.load(open(filelist_file, 'r'))
+
+result = {}
+for track, path in filelist.iteritems():
+    if track in gt['groundTruth']:
+        result[track] = path
+
+with open(results_filelist_file, 'w') as f:
+    yaml.dump(result, f)
+
