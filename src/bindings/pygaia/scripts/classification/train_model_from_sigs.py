98333d66bad7d2f5dcd9aff72b4c5f7944a1a0f5
Pablo Alonso <pablo.alonso@upf.edu>
2019-02-19 13:48:22 +0100

    Improved train_model_from_sigs.py behavior
    - Be able to create a project from sig files
    - Be able to store generated files in a different folder
    - Check that every json file had been converted (not only amount of files)

--- src/bindings/pygaia/scripts/classification/train_model_from_sigs.py
+++ src/bindings/pygaia/scripts/classification/train_model_from_sigs.py
@@ -5,9 +5,10 @@ import json_to_sig
 import gaia2.fastyaml as yaml
 import sys
 import os
+import shutil
 import json
 import glob
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 template = {"type": "singleClass",
             "version": 1.0,
@@ -18,69 +19,87 @@ template = {"type": "singleClass",
 def get_files_in_dir(dirname, extension):
     return glob.glob(os.path.join(dirname, "*.%s" % extension))
 
-def main(dirname, options):
-    print "running in dir", dirname
-    project_dir = os.path.abspath(dirname)
-    projname = os.path.basename(dirname)
+def main(input_directory, output_directory, force=False, project_name=None):
+    print "looking for data in dir", input_directory
+    print "storing resutls in dir", output_directory
+
+
+    project_dir = os.path.abspath(input_directory)
+
+    if not project_name:
+        projname = os.path.basename(input_directory)
+    else:
+        projname = project_name
+
+    output_dir = os.path.abspath(output_directory)
 
     # if config/results exist, need force to rm them
-    project_file = os.path.join(project_dir, "%s.project" % projname)
-    results_model_file = os.path.join(project_dir, "%s.history" % projname)
-    resultsdir = os.path.join(project_dir, "results")
+    project_file = os.path.join(output_dir, "%s.project" % projname)
+    results_model_file = os.path.join(output_dir, "%s.history" % projname)
+    resultsdir = os.path.join(output_dir, "results")
     datasetsdir = os.path.join(project_dir, "datasets")
 
-    if os.path.exists(resultsdir):
-        print >> sys.stderr, "Results directory already exists. Use -f to delete and re-run"
-        return
+    if not os.path.exists(resultsdir):
+        os.makedirs(resultsdir)
+
 
     classes = [d for d in os.listdir(project_dir) \
             if os.path.isdir(os.path.join(project_dir, d))]
     print classes
 
-    groundtruth_name = os.path.join(project_dir, "groundtruth.yaml")
-    json_name = os.path.join(project_dir, "filelist.yaml")
-    yaml_name = os.path.join(project_dir, "filelist-yaml.yaml")
+    groundtruth_name = os.path.join(resultsdir, "groundtruth.yaml")
+    json_name = os.path.join(resultsdir, "filelist-to-convert.yaml")
+    yaml_name = os.path.join(resultsdir, "filelist-yaml.yaml")
 
     filelist = {}
     groundtruth = template
-    missingsig = False
+
     for c in classes:
-        files = get_files_in_dir(os.path.join(project_dir, c), "json")
+        jsonfiles = get_files_in_dir(os.path.join(project_dir, c), "json")
         yamlfiles = get_files_in_dir(os.path.join(project_dir, c), "sig")
 
-        if len(files) != len(yamlfiles):
-            missingsig = True
+        yamlfilesNoExt = [f.rstrip('.sig') for f in yamlfiles]
 
-        print "got", len(files), "files in", c
-        for f in files:
+        if (len(jsonfiles) > 0):
+            filesToConvert = {os.path.splitext(os.path.basename(f))[0]: os.path.join(
+                project_dir, c, f) for f in jsonfiles if f.rstrip('.json') not in yamlfilesNoExt}
+
+            print "{} json files have to be converted into yamls. {} already exist.".format(len(filesToConvert), len(yamlfiles))
+
+
+            yaml.dump(filesToConvert, open(json_name, "w"))
+            json_to_sig.convertJsonToSig(json_name, yaml_name)
+
+        yamlfiles = get_files_in_dir(os.path.join(project_dir, c), "sig")
+
+        print "got", len(yamlfiles), "files in", c
+        for f in yamlfiles:
             id = os.path.splitext(os.path.basename(f))[0]
             groundtruth["groundTruth"][id] = c
             filelist[id] = os.path.join(project_dir, c, f)
 
+
     # check directories for sig and convert
     groundtruth["className"] = projname
-    yaml.dump(filelist, open(json_name, "w"))
+    yaml.dump(filelist, open(yaml_name, "w"))
     yaml.dump(groundtruth, open(groundtruth_name, "w"))
 
-    if missingsig:
-        print "converting sig"
-        json_to_sig.convertJsonToSig(json_name, yaml_name)
+    if os.path.exists(json_name):
+        os.remove(json_name)
 
-    # run
-    train_model.trainModel(groundtruth_name, yaml_name, project_file, project_dir, results_model_file)
+    train_model.trainModel(groundtruth_name, yaml_name, project_file, resultsdir, results_model_file)
 
 if __name__ == "__main__":
-    parser = OptionParser(usage = '%prog directory\n' +
-"""
-Generates a model of the sig files in `directory`. Converts to yaml
-if needed.
-""")
+    parser = ArgumentParser()
+    # parser.description("Generates a model of the sig files in `directory`. Converts to yaml if needed.")
+    parser.add_argument('input_directory', help='directory with the json/sig files.')
+    parser.add_argument('output_directory', help='directory with the output files.')
+    parser.add_argument('--force', '-f', help='directory with the output files.', action='store_true')
+    parser.add_argument('--project_name', '-n', help='the project name.')
 
-    options, args = parser.parse_args()
-
-    try:
-        directory = args[0]
-    except:
-        parser.print_help()
-        sys.exit(1)
-    main(directory, options)
+    args = parser.parse_args()
+    input_directory = args.input_directory
+    output_directoty = args.output_directory
+    force = args.force
+    project_name = args.project_name
+    main(input_directory, output_directoty, force, project_name)
