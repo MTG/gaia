11036e0437dbf022953153eeaa4b46b9ad871b4e
pabloEntropia <pablo.alonso@upf.edu>
2020-03-09 19:57:42 +0100

    Add specific project templates and choosing system
    The classification project template can be specified as an input
    argument. If the argument is not given, the algorithm will read the
    essentia version from the sig files and try to load the appropriate
    template.
    
    Aditionally, optparser was replaced by argparser to better support the
    new command line options.

--- src/bindings/pygaia/scripts/classification/generate_classification_project.py
+++ src/bindings/pygaia/scripts/classification/generate_classification_project.py
@@ -23,13 +23,22 @@ from __future__ import print_function
 from __future__ import with_statement
 import sys
 from os.path import join, abspath
-from optparse import OptionParser
+from argparse import ArgumentParser
 from gaia2 import filedir
 import gaia2.fastyaml as yaml
 
-PROJECT_TEMPLATE = open(join(filedir(), 'classification_project_template.yaml')).read()
 
-def generateProject(groundtruth_file, filelist_file, project_file, datasets_dir, results_dir, seed=None, cluster_mode=False):
+# Use a dict to map essentia versions to the appropriate template so that we
+# don't need to store one template for each version.
+VERSION_MAP = {
+    '2.1-beta5': '2.1-beta5',
+    '2.1-beta6': '2.1-beta5',
+}
+
+
+def generateProject(groundtruth_file, filelist_file, project_file, datasets_dir,
+                    results_dir, seed=None, cluster_mode=False, template=None):
+
     gt = yaml.load(open(groundtruth_file, 'r'))
     try:
         className = gt['className']
@@ -57,6 +66,33 @@ def generateProject(groundtruth_file, filelist_file, project_file, datasets_dir,
         print("track ids found in", groundtruth_file, "are inconsistent with", filelist_file)
         sys.exit(4)
 
+    print('Analizing the dataset to figure out which project template file to use...')
+
+    essentia_version = ''
+    if not template:
+        try:
+            versions = set([yaml.load(open(v))['metadata']['version']['essentia'] for v in fl.values()])
+
+            if (len(versions) == 1):
+                parsed_version = list(versions)[0].split('-')
+
+                essentia_version = parsed_version[0]
+
+                if parsed_version[1].startswith('beta'):
+                    essentia_version += '-{}'.format(parsed_version[1])
+            else:
+                print('More than one essentia versions found in the datataset.\n'
+                      'Using the default project template.')
+        except IOError:
+            print('Error retrieving essentia version')
+
+    template_version = VERSION_MAP.get(essentia_version, 'default')
+
+    print('Using classification project template "{}"'.format(template_version))
+    template = 'classification_project_template_{}.yaml'.format(template_version)
+
+    project_template = open(join(filedir(), template)).read()
+
     # If not seed specified, get the current clock value
     if seed is None:
         import time
@@ -64,7 +100,7 @@ def generateProject(groundtruth_file, filelist_file, project_file, datasets_dir,
 
     # write the project file
     with open(project_file, 'w') as pfile:
-        pfile.write(PROJECT_TEMPLATE % { 'className': className,
+        pfile.write(project_template % { 'className': className,
                                          'datasetsDirectory': abspath(datasets_dir),
                                          'resultsDirectory': abspath(results_dir),
                                          'filelist': abspath(filelist_file),
@@ -76,19 +112,27 @@ def generateProject(groundtruth_file, filelist_file, project_file, datasets_dir,
 
 
 if __name__ == '__main__':
-    parser = OptionParser(usage = '%prog [options] groundtruth_file filelist_file project_file datasets_dir results_dir')
+    parser = ArgumentParser()
 
-    options, args = parser.parse_args()
+    parser.add_argument('groundtruth_file')
+    parser.add_argument('filelist_file')
+    parser.add_argument('project_file')
+    parser.add_argument('datasets_dir')
+    parser.add_argument('results_dir')
+    parser.add_argument('-s', '--seed', default=None)
+    parser.add_argument('-c', '--cluster_mode', action='store_true')
+    parser.add_argument('-t', '--template', default=None)
 
-    try:
-        groundtruth_file = args[0]
-        filelist_file = args[1]
-        project_file = args[2]
-        datasets_dir = args[3]
-        results_dir = args[4]
-    except:
-        parser.print_help()
-        sys.exit(1)
+    args = parser.parse_args()
 
-    generateProject(groundtruth_file, filelist_file, project_file, datasets_dir, results_dir)
+    groundtruth_file = args.groundtruth_file
+    filelist_file = args.filelist_file
+    project_file = args.project_file
+    datasets_dir = args.datasets_dir
+    results_dir = args.results_dir
+    seed = args.seed
+    cluster_mode = args.cluster_mode
+    template = args.template
 
+    generateProject(groundtruth_file, filelist_file, project_file, datasets_dir,
+                    results_dir, seed=seed, cluster_mode=cluster_mode, template=template)
