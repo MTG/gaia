25cb7a69fad8f96df7d606b042cad670e5572b73
Alastair Porter <alastair.porter@upf.edu>
2020-03-19 13:45:23 +0100

    Replace GVarLengthArray with QVarLengthArray
    This was originally in use, but replaced because of incompatibilities in
    an old version of QT. No longer needed

--- src/algorithms/svmpredict.cpp
+++ src/algorithms/svmpredict.cpp
@@ -1,18 +1,18 @@
-/* 
+/*
  * Copyright (C) 2006-2013  Music Technology Group - Universitat Pompeu Fabra
  *
  * This file is part of Gaia
- * 
- * Gaia is free software: you can redistribute it and/or modify it under 
- * the terms of the GNU Affero General Public License as published by the Free 
- * Software Foundation (FSF), either version 3 of the License, or (at your 
+ *
+ * Gaia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
  * option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the Affero GNU General Public License
  * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
  */
@@ -78,7 +78,7 @@ Point* SVMPredict::svmPredict(const Point* p, Point* result,
     // predict class (and probabilities)
     double predictedClass;
     if (_probability) {
-      GVarLengthArray<double, 32> classProbability(_classMapping.size());
+      QVarLengthArray<double, 32> classProbability(_classMapping.size());
       predictedClass = svm_predict_probability(model, x, classProbability.data());
 
       Real* probs = 0;
